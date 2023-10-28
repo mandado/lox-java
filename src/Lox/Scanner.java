3a642fb515fb89f3cd31c43a4a4ba040b7ad6b67
Jorge Roberto <jorgerobertodev@gmail.com>
2023-10-28 16:07:22 -0300

    added reserved keywords to scanner added new scan tokens added some comments to remember what the code do on match

--- src/Lox/Scanner.java
+++ src/Lox/Scanner.java
@@ -1,7 +1,10 @@
 package Lox;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner {
     private final String source;
@@ -9,6 +12,29 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    TokenType.AND);
+        keywords.put("class",  TokenType.CLASS);
+        keywords.put("else",   TokenType.ELSE);
+        keywords.put("false",  TokenType.FALSE);
+        keywords.put("for",    TokenType.FOR);
+        keywords.put("fun",    TokenType.FUN);
+        keywords.put("if",     TokenType.IF);
+        keywords.put("nil",    TokenType.NIL);
+        keywords.put("or",     TokenType.OR);
+        keywords.put("print",  TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super",  TokenType.SUPER);
+        keywords.put("this",   TokenType.THIS);
+        keywords.put("true",   TokenType.TRUE);
+        keywords.put("var",    TokenType.VAR);
+        keywords.put("while",  TokenType.WHILE);
+    }
+
 
     public Scanner(String source) {
         this.source = source;
@@ -32,6 +58,7 @@ public class Scanner {
         char c = advance();
 
         switch (c) {
+            //
             case '(':
                 addToken(TokenType.LEFT_PAREN);
                 break;
@@ -59,17 +86,133 @@ public class Scanner {
             case '*':
                 addToken(TokenType.STAR);
                 break;
-            case '!' :
+            case '!':
                 addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                break;
+            case '=':
+                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(TokenType.SLASH);
+                }
+            case '<':
+                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                break;
+            case '>':
+                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
                 break;
             case ';':
                 addToken(TokenType.SEMICOLON);
                 break;
 
+            case ' ':
+            case '\r':
+            case '\t':
+                // skip whitespace.
+                break;
+
+            // go to next line
+            case '\n':
+                line++;
+                break;
+
+                // string type
+            case '"': string(); break;
+
             default:
-                Lox.error(line, "unexpected character.");
+                if (isDigit(c)) {
+                    number();
+                } else if(isAlpha(c)) {
+                    identifier();
+                }
+                else {
+                    Lox.error(line, "unexpected character.");
+                }
                 break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphanumeric(peek())) advance();
+
+        System.out.println(MessageFormat.format("{0} --- {1}", start, current));
+        String text = source.substring(start, current);
+        System.out.println("identifier: " + text);
+        TokenType type = keywords.get(text);
+        if (type == null) type = TokenType.IDENTIFIER;
+
+        addToken(type);
+    }
+
+    private boolean isAlphanumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isAlpha(char c) {
+        return Character.isAlphabetic(c) || c == '_';
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+
+        return source.charAt(current + 1);
+    }
+
+    private boolean isDigit(char c) {
+        return Character.isDigit(c);
+    }
+
+    private void string() {
+        while(peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string");
+            return;
+        }
+
+        advance(); // close quotes ".
+
+        String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+
+        return source.charAt(current);
+    }
+
+    /*
+     *  var a = b != 1;
+     *  the advance() method works as follows, it returns the current and increments one more.
+     *  so, when the current is 10, and you check current variable value next time, the value will be 11.
+     *  returning the next char, matching with the expected char and returning true.
+     * */
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
     }
 
     private void addToken(TokenType type) {
